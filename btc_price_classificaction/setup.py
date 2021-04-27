d95b6d459fcde39b3e07c27502be9db792e80c67
morenotos <julio.mc2@gmail.com>
2021-04-27 18:22:13 -0500

    First commit. Created 4 notebooks for data wrangling and EDA in folder notebooks

---
+++ btc_price_classificaction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard capstopne 3 project - make a classification model for bitcoin price movements and technical indicators.',
+    author='Julio',
+    license='MIT',
+)
